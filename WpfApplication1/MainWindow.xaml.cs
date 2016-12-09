41f4b05c0f968f693bd9272a2608c1a3d2981cc8
airkhan <erk.k@gmx.net>
2016-12-09 10:14:57 +0100

    First receive of data from server

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LibraryAdminService lib = new LibraryAdminService("http://mge7.dev.ifs.hsr.ch/");
+
+            List<Loan> loans = lib.GetAllLoans();
+
+            dgLoans.ItemsSource = loans;
+
+            List<TestVO> list1 = new List<TestVO>();
+            list1.Add(new TestVO() { KundenNr = 1, Name = "John Doe", Reservation = "iphone 7", Ausleihen = "iphone, blablub" });
+
+
+        }
+        public class TestVO
+        {
+            public int KundenNr { get; set; }
+
+            public string Name { get; set; }
+
+            public string Reservation { get; set; }
+
+            public string Ausleihen { get; set; }
+
+        }
+    }
+}
