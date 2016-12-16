898ad7077374a49117de3b65d13dc241f0103759
airkhan <erk.k@gmx.net>
2016-12-16 14:18:31 +0100

    kunde

--- WpfApplication1/Kunde.xaml.cs
+++ WpfApplication1/Kunde.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +23,38 @@ namespace WpfApplication1
     /// </summary>
     public partial class Kunde : UserControl
     {
+
+
+        public ObservableCollection<Loan> loans { get; set; }
+
+
         public Kunde()
         {
             InitializeComponent();
+            var url = "http://mge7.dev.ifs.hsr.ch/";
+
+            LibraryAdminService lib = new LibraryAdminService(url);
+
+            loans = new ObservableCollection<Loan>(lib.GetAllLoans());
+
+           
+            // web socket connection to listen to changes:
+            var client = new ch.hsr.wpf.gadgeothek.websocket.WebSocketClient(url);
+            client.NotificationReceived += (o, e) =>
+            {
+                // demonstrate how these updates could be further used
+                if (e.Notification.Target == typeof(Loan).Name.ToLower())
+                {
+                    loans = new ObservableCollection<Loan>(lib.GetAllLoans());
+                }
+            };
+
+            // spawn a new background thread in which the websocket client listens to notifications from the server
+            client.ListenAsync();
+
+
+            DataContext = this;
+            
         }
     }
 }
